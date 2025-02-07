44e03c25d6be4c2485ca9418a264d410684e68c2
Gobro-s <gobro6366@gmail.com>
2025-02-08 02:08:20 +0900

    soda ing2

--- DFS_BFS/frozenSoda.py
+++ DFS_BFS/frozenSoda.py
@@ -12,8 +12,25 @@ print(graph)
 
 def dfs(x, y):
     # 범위 밖 제외
-    if x <= 0 or x > n or y < 0 or y >= m:
+    if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
     # 방문 처리
     if graph[x][y] == 0:
         graph[x][y] = 1
+        # 상하좌우 호출
+        dfs(x - 1, y)
+        dfs(x, y - 1)
+        dfs(x + 1, y)
+        dfs(x, y + 1)
+        return False
+    return False
+
+answer = 0
+# 음료 채우기
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j):
+            answer += 1
+
+print(f'answer 는 {answer}')
+
